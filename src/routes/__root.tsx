7ff0954e8ce023da9b32e6e94214a07d6fc9e0ff
Taump <taump@yandex.ru>
2026-03-24 18:13:58 +0100

    chore(components): remove unused components and styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,6 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-
-import StoreDevtools from '../lib/demo-store-devtools'
 
 import TanStackQueryProvider from '../integrations/tanstack-query/root-provider'
 
@@ -30,9 +26,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 function RootLayout() {
   return (
     <TanStackQueryProvider>
-      <Header />
       <Outlet />
-      <Footer />
       <TanStackDevtools
         config={{
           position: 'bottom-right',
@@ -42,7 +36,6 @@ function RootLayout() {
             name: 'Tanstack Router',
             render: <TanStackRouterDevtoolsPanel />,
           },
-          StoreDevtools,
           TanStackQueryDevtools,
         ]}
       />
