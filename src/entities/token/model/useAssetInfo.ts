71ffa19378304045ff44ffa07d3090f14ce16086
Alexander Ponomarev <taump@yandex.ru>
2026-04-11 15:13:54 +0200

    fix(useAssetInfo): handle optional chaining for asset metadata

--- src/entities/token/model/useAssetInfo.ts
+++ src/entities/token/model/useAssetInfo.ts
@@ -5,10 +5,10 @@ import { assetMetadataStore } from "./store";
 export function useAssetInfo(coopAsset: string | undefined) {
   const assetMetadata = useStore(assetMetadataStore, (s) => s);
 
-  const coopDecimals = coopAsset ? (assetMetadata[coopAsset].decimals ?? 0) : 0;
+  const coopDecimals = coopAsset ? (assetMetadata[coopAsset]?.decimals ?? 0) : 0;
   const gbyteDecimals = assetMetadata["base"].decimals ?? 9;
   const coopSymbol = coopAsset
-    ? (assetMetadata[coopAsset].symbol ?? "COOP")
+    ? (assetMetadata[coopAsset]?.symbol ?? "COOP")
     : "COOP";
 
   return { coopAsset, coopDecimals, gbyteDecimals, coopSymbol };
